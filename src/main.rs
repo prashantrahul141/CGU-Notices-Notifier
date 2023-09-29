b6b408ec315054b89ad9bd4579a653d301052a43
Prashant <prashantrahul141@protonmail.com>
2023-09-30 02:12:04 +0530

    use tokio to mark main async, parse envs and get site html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod parse_env;
+mod site_scraper;
+
+#[macro_use]
+extern crate log;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    info!("Starting up");
+    let env_vars: parse_env::Config = parse_env::parse_env();
+    let site_html_result = site_scraper::get_site_html(&env_vars.site_url).await;
 }
