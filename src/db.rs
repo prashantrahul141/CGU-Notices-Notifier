8d3eb0082b536ed0bae672f91643f226fdc36111
Prashant <prashantrahul141@protonmail.com>
2023-10-02 02:18:15 +0530

    return document from db function

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,22 @@
 #![allow(dead_code)]
-use mongodb::{options::ClientOptions, sync::Client};
+use crate::structs;
+use mongodb::{bson::doc, options::ClientOptions, sync::Client};
 
 ///
-pub fn mongo_client_init(connection_uri: &String) -> Client {
+pub fn get_document(
+    connection_uri: &String,
+    database_name: &String,
+    collection_name: &String,
+) -> structs::DbCollectionType {
+    info!("Creating db.");
     let client_options = ClientOptions::parse(connection_uri);
     let client = Client::with_options(client_options.unwrap())
         .expect("Failed to establish connection to the db.");
-    client
+    let db_con = client.database(database_name);
+    let data_col = db_con.collection::<structs::DbCollectionType>(collection_name);
+
+    let filter = doc! { "data_id": "cgu-data-id"};
+    info!("Finding document.");
+    let found_document = data_col.find_one(Some(filter), None).unwrap().unwrap();
+    found_document
 }
