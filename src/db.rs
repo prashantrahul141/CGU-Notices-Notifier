75e5518363887a04ae62dc70ff89bc8ab4149a2d
Prashant <prashantrahul141@protonmail.com>
2023-10-03 22:06:10 +0530

    update error message to be more clear

--- src/db.rs
+++ src/db.rs
@@ -8,19 +8,22 @@ pub async fn get_document(
     collection_name: &String,
 ) -> structs::DbCollectionType {
     info!("Creating db.");
+
     let client_options = ClientOptions::parse(connection_uri);
     let client = Client::with_options(client_options.await.unwrap())
         .expect("Failed to establish connection to the db.");
+
     let db_con = client.database(database_name);
     let data_col = db_con.collection::<structs::DbCollectionType>(collection_name);
 
     let filter = doc! { "data_id": "cgu-data-id"};
     info!("Finding document.");
+
     let found_document = data_col
         .find_one(Some(filter), None)
         .await
-        .expect("Failed to establish connection to db.")
-        .expect("Failed to establish connection to db.");
+        .expect("failed to parse collection.")
+        .expect("failed to find exact document in collection.");
 
     found_document
 }
