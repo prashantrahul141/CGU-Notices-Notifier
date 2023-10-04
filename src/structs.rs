526308123d00c9fcac14367e9f0691fb6edcbc3f
Prashant <prashantrahul141@protonmail.com>
2023-10-04 18:59:19 +0530

    make Derivce Clone for NoticeElement and DbMetaData, add utc time field in NoticeElement

--- src/structs.rs
+++ src/structs.rs
@@ -1,15 +1,17 @@
+use chrono::DateTime;
 use serde::{Deserialize, Serialize};
 
 // Struct to hold each notice.
-#[derive(Hash, Debug, Serialize, Deserialize)]
+#[derive(Hash, Debug, Serialize, Deserialize, Clone)]
 pub struct NoticeElement {
     pub hash: String,
     pub title: String,
     pub date: String,
+    pub utc_time: DateTime<chrono::Utc>,
     pub file_url: String,
 }
 
-#[derive(Hash, Debug, Serialize, Deserialize)]
+#[derive(Hash, Debug, Serialize, Deserialize, Clone)]
 pub struct DbMetaData {
     pub latest_hash: String,
     pub subscribed_users: Vec<String>,
