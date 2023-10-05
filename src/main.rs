bcb24e354ca8721849481b5e0ad204ca3ea27c42
Prashant <prashantrahul141@protonmail.com>
2023-10-05 19:55:10 +0530

    using bot loop and notify loop in main, also importing external crates

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
+mod bot;
+mod db;
+mod notifier;
 mod parse_env;
 mod site_scraper;
+mod structs;
 
 #[macro_use]
 extern crate log;
+extern crate chrono;
+extern crate urlencoding;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
-
     info!("Starting up");
+
     let env_vars: parse_env::Config = parse_env::parse_env();
-    let site_html_result = site_scraper::get_site_html(&env_vars.site_url).await;
+    let bot_loop = async {
+        bot::run(
+            &env_vars.teloxide_token,
+            &env_vars.db_connection_uri,
+            &env_vars.database_name,
+        )
+        .await;
+    };
+
+    let notify_loop = async {
+        notifier::notify_loop(
+            &env_vars.db_connection_uri,
+            &env_vars.database_name,
+            &env_vars.site_url,
+            &env_vars.teloxide_token,
+        )
+        .await;
+    };
+
+    tokio::join!(bot_loop, notify_loop);
 }
