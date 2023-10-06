cfdfc5f3782b4e5fde0881bdce2a6588c219f963
Prashant <prashantrahul141@protonmail.com>
2023-10-06 11:43:30 +0530

    add: constants.rs file

---
+++ src/constants.rs
@@ -0,0 +1,9 @@
+pub const START_MESSAGE: &str =  "Greetings👋\nWould you like to subscribe to CGU notices notifications?
+            \nI will send you the notice everytime CGU posts any update on their website's notice board 😸";
+
+pub const SUBSCRIBE_MESSAGE: &str = "Congratulations🎉\nYou\'re now subscribed to cgu \
+                    notices notifications, I will send you the notice everytime CGU \
+                    posts something on their website notice board 😼";
+
+pub const UNSUBSCRIBE_MESSAGE: &str = "You\'ve been unsubscribed from CGU notices notifications, \
+                    I won\'t send you notices anymore 😿\nTo subscribe again just send /subscribe.";
