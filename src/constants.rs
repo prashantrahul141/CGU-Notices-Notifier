e19191cce65e3d1cfd80ebdabb5c8a828bf01ba2
Prashant <prashantrahul141@protonmail.com>
2023-10-08 15:25:31 +0530

    add: INFO_MESSAGE

--- src/constants.rs
+++ src/constants.rs
@@ -7,3 +7,12 @@ pub const SUBSCRIBE_MESSAGE: &str = "Congratulations🎉\nYou\'re now subscribed
 
 pub const UNSUBSCRIBE_MESSAGE: &str = "You\'ve been unsubscribed from CGU notices notifications, \
                     I won\'t send you notices anymore 😿\nTo subscribe again just send /subscribe.";
+
+pub const INFO_MESSAGE: &str =
+    "Hello! My only purpose is to send you notifications whenever CGU posts any updates on their \
+                  website's notice board.\nSince it doesn\'t have a system where it can notify students on updates,\
+                   it creates quite a lot of confusion among the students.\nI stand to solve that problem by notifying everyone who is \
+                   subscribed to me within 10 minutes.\nYou can subscribe too, just by sending a /subscribe to me. \
+                   And you can unsubscibe from me anytime you want!\n\n \
+                   Here's the source code if you\'re interested : https://github.com/prashantrahul141/CGU-Notices-Notifier\
+                   \nIf you like the project consider giving it a star!";
